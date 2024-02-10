bf9b729479f8a223dcde7fbed1bd64a5038e8dd6
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-02-10 20:25:39 +0100

    fix: :art: reset migrations, added dockerfile

--- prisma/migrations/20240209181707_cinema_model2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `cinema` MODIFY `phone` VARCHAR(191) NULL;
--- prisma/migrations/20240210192414_init/migration.sql
+++ prisma/migrations/20240210192414_init/migration.sql
@@ -15,6 +15,12 @@ CREATE TABLE `User` (
 CREATE TABLE `Cinema` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
+    `address1` VARCHAR(191) NOT NULL,
+    `address2` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `postalCode` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 

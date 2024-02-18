020d90afa2dfc3294ed842608a404150aa84b4e0
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-02-18 11:22:41 +0100

    ci: :construction: test deploy dev backend

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, everybody\'s World!';
+    return 'Hello, experimental world!';
   }
 }

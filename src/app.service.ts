0add7e2de3e9edb8e878f53dcf8e5165f502a1ca
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-02-18 19:44:37 +0100

    ci: :construction: test webhook trigger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, everybody\'s world!';
+    return '<h1>Hello, everybody\'s world!</h1>';
   }
 }

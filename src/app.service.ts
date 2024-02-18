492744a3aeeaba7cdbacd9f19f0e2382df9db141
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-02-18 19:25:33 +0100

    refactor: :speech_balloon: Set welcome message for prod

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, experimental world!';
+    return 'Hello, everybody\'s world!';
   }
 }

96b901b181f2b51b4436f1427315b7d3120fe328
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-03-23 19:32:15 +0100

    feat: :sparkles: prefs in GET/users/me, can post prefs.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Request, Get, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get, UseGuards, Post, Body, Put } from '@nestjs/common';
 import { ApiTags, ApiOkResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { MineUserEntity } from './entities/mineUser.etity';
 import { UserEntity } from './entities/user.entity';
+import { PrefEntity } from './entities/prefs.entity';
+import { PrefDTO } from './DTO/pref.dto';
+import { PrefUpdateDTO } from './DTO/prefUpdate.dto';
 
 @Controller('users')
 @ApiTags('users')
@@ -22,4 +25,21 @@ export class UsersController {
     async getMe(@Request() req): Promise<MineUserEntity> {
         return await this.userService.getMe(req.user.id);
     }
+
+    @Post('prefs')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({ type: PrefEntity })
+    async createPref(@Request() req, @Body() pref: PrefDTO): Promise<PrefEntity> {
+        return await this.userService.createPref(req.user.id, pref);
+    }
+
+    @Put('prefs/:prefId')
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({ type: PrefEntity })
+    async updatePref(@Request() req, @Body() pref: PrefUpdateDTO): Promise<PrefEntity> {
+        return await this.userService.updatePref(req.user.id, pref);
+    }
+    
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserEntity } from './entities/user.entity';
 import { MineUserEntity } from './entities/mineUser.etity';
+import { PrefEntity } from './entities/prefs.entity';
+import { PrefDTO } from './DTO/pref.dto';
+import { PrefUpdateDTO } from './DTO/prefUpdate.dto';
 
 @Injectable()
 export class UsersService {
@@ -16,6 +19,14 @@ export class UsersService {
     }
 
     async getMe(id: number): Promise<MineUserEntity> {
-        return await this.prisma.user.findUnique({where: {id}, select:{id: true, pseudo: true, email: true}});
+        return await this.prisma.user.findUnique({where: {id}, select:{id: true, pseudo: true, email: true, prefs: {select: {id: true, name: true, theme: true, images: true, audio: true, helpLevel: true}}}});
+    }
+
+    async createPref(id: number, body: PrefDTO): Promise<PrefEntity> {
+        return await this.prisma.prefs.create({data: {userId: id, ...body}});
+    }
+
+    async updatePref(id: number, body: PrefUpdateDTO): Promise<PrefEntity> {
+        return await this.prisma.prefs.update({where: {id}, data: body});
     }
 }

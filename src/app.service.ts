ddfabe09cbb0a9f0a2bdb1fd0e5fece3d7b69aa6
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-04-13 19:35:38 +0200

    ci: :green_heart: Clean code after eslint tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    let test = 'test';
-    console.log('test');
     return 'Hello, my experimental world!';
   }
 }

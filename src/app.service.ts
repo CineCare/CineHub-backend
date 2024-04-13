a60d6bf9ae4964db489f2cd014d8cb848ae30ad3
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-04-13 19:30:16 +0200

    ci: :test_tube: Test build with checkStyle error

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    let test = 'test';
     console.log('test');
     return 'Hello, my experimental world!';
   }

8eb7e85724424347b5327efb4462335e4035b227
WhitedogWarren <werquin.lucas@wanadoo.fr>
2024-06-22 18:38:26 +0200

    test: re test jenkins

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, my experimental world!';
+    return 'Hello, my technical world!';
   }
 }
